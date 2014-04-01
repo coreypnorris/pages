6f6dda9d5eecf72aec216cb6c589b8a2e4013cf4
Corey Norris <coreypnorris@gmail.com>
2014-04-01 16:05:14 -0700

    allows users to create, read, and list contacts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pages::Application.config.secret_key_base = '540144763d50735fe93ab110ae9e0128ac86a784e48c8e79eae9330a9e830ee6f0a97e0b7baa5dc0c4308986f53c10cf13739eab300b5b42796ba9a84677d327'
